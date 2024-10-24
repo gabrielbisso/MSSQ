81bdb05fcf9a410d67e7c5748831c27b0a2ded7e
Maria Eduarda Alves Gonzalez <104467663+meagonzalez@users.noreply.github.com>
2024-10-24 16:38:37 -0300

    Esqueleto do Projeto

---
+++ MSSQShort/quest2/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Quest2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'quest2'
---
+++ MSSQShort/quest3/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Quest3Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'quest3'
