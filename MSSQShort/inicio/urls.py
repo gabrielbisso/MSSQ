aabc35e970c2e7b154b3fa22ec7f0b848836fa26
Maria Eduarda Alves Gonzalez <104467663+meagonzalez@users.noreply.github.com>
2024-11-14 17:24:47 -0300

    Update urls.py

--- MSSQShort/inicio/urls.py
+++ MSSQShort/inicio/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('initial', views.index, name="initial"),
+    path('', views.index, name="home"),  # Now serves as the root URL for 'inicio'
+    #path('initial', views.index, name="initial"),
     path('loginAdm', views.login_Adm, name="loginAdm"),
     path('loginUser', views.login_User, name="loginUser")
 ]
