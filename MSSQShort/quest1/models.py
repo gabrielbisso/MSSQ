77126b1a0094c6bf7795160270a36899651e484f
Nicole Redmann <nickred2004@gmail.com>
2024-11-18 20:05:32 -0300

    models_quest1

--- MSSQShort/quest1/models.py
+++ MSSQShort/quest1/models.py
@@ -2,12 +2,26 @@ from django.db import models
 
 class quest1(models.Model):
     idade = models.IntegerField()
-    sexo = models.FloatField(null=True, blank=True)
-    nao_aplicavel =  models.FloatField(null=True, blank=True)
-    nunca = models.FloatField(null=True, blank=True)
-    raramente = models.FloatField(null=True, blank=True)
-    algumas_vezes = models.FloatField(null=True, blank=True)
-    frequentemente = models.FloatField(null=True, blank=True)
+    sexo = models.IntegerField(null=True, blank=True)
+    carros =  models.IntegerField(null=True, blank=True)
+    autocarros = models.IntegerField(null=True, blank=True)
+    comboios = models.IntegerField(null=True, blank=True)
+    aviões = models.IntegerField(null=True, blank=True)
+    embarcacao_pequena = models.IntegerField(null=True, blank=True)
+    navios = models.IntegerField(null=True, blank=True)
+    baloiços = models.IntegerField(null=True, blank=True)
+    carroseis = models.IntegerField(null=True, blank=True)
+    montanha_russa = models.IntegerField(null=True, blank=True)
+    
+    carros10 =  models.IntegerField(null=True, blank=True)
+    autocarros10 = models.IntegerField(null=True, blank=True)
+    comboios10 = models.IntegerField(null=True, blank=True)
+    aviões10 = models.IntegerField(null=True, blank=True)
+    embarcacao_pequena10 = models.IntegerField(null=True, blank=True)
+    navios10 = models.IntegerField(null=True, blank=True)
+    baloiços10 = models.IntegerField(null=True, blank=True)
+    carroseis10 = models.IntegerField(null=True, blank=True)
+    montanha_russa10 = models.IntegerField(null=True, blank=True)
     
     def __str__(self) -> str:
         return self.name
