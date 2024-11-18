2b8ddd505167dcabaf11395b4708e4d8f8901495
Nicole Redmann <nickred2004@gmail.com>
2024-11-18 18:48:43 -0300

    models_quest1

--- MSSQShort/quest1/models.py
+++ MSSQShort/quest1/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-
+class quest1(models.Model):
+    idade = models.IntegerField()
+    sexo = models.FloatField(null=True, blank=True)
+    nao_aplicavel =  models.FloatField(null=True, blank=True)
+    nunca = models.FloatField(null=True, blank=True)
+    raramente = models.FloatField(null=True, blank=True)
+    algumas_vezes = models.FloatField(null=True, blank=True)
+    frequentemente = models.FloatField(null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return self.name
+ 
