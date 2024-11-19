f3a00833a981dd9f42bbe08c3ebeed2c5d84cd60
Gabriel Bisso <gbissofagundes@gmail.com>
2024-11-19 10:42:47 -0300

    breve ajuste da lógica do banco

--- MSSQShort/login/views.py
+++ MSSQShort/login/views.py
@@ -7,6 +7,8 @@ from quest1.models import banco_quest1
 from quest2.models import banco_quest2
 from quest3.models import banco_quest3
 
+credencialglobal = None
+
 
 def login_Adm(request):
     if request.method == "GET":
@@ -25,6 +27,8 @@ def login_Adm(request):
 
 
 def login_User(request):
+    global credencialglobal
+
     if request.method == "GET":
         disable_buttonfalse = False
         disable_button = True
@@ -32,6 +36,7 @@ def login_User(request):
 
     elif request.method == "POST":
         credencial = request.POST.get('credencial')
+
         # Verificando a existência dos registros nos três bancos de dados
         q1 = banco_quest1.objects.filter(id=credencial).first()
         q2 = banco_quest2.objects.filter(id=credencial).first()
@@ -43,60 +48,64 @@ def login_User(request):
         q3_exist = q3 is not None
 
         # Se a credencial estiver vazia, desabilita todos os botões
-        if credencial == "":
+        if credencial == "":  
             disable_button = True
             return render(request, 'loginUser.html', {
-                'disable_button1': disable_button,
-                'disable_button2': disable_button,
-                'disable_button3': disable_button,
+                'disable_button1': disable_button, 
+                'disable_button2': disable_button, 
+                'disable_button3': disable_button, 
                 'credencial': credencial
             })
 
         # Se q1 não existe, desabilita o botão correspondente
-        elif not q1_exist:
+        elif not q1_exist: 
+            credencialglobal = credencial 
             disable_buttonfalse = False
             disable_button = True
             return render(request, 'loginUser.html', {
-                'disable_button1': disable_buttonfalse,
-                'disable_button2': disable_button,
-                'disable_button3': disable_button,
+                'disable_button1': disable_buttonfalse, 
+                'disable_button2': disable_button, 
+                'disable_button3': disable_button, 
                 'credencial': credencial
             })
 
         # Se q1 existe mas q2 não, desabilita o botão de q2
-        elif q1_exist and not q2_exist:
+        elif q1_exist and not q2_exist:  
+            credencialglobal = credencial
             disable_buttonfalse = False
             disable_button = True
             return render(request, 'loginUser.html', {
-                'disable_button1': disable_button,
-                'disable_button2': disable_buttonfalse,
-                'disable_button3': disable_button,
+                'disable_button1': disable_button, 
+                'disable_button2': disable_buttonfalse, 
+                'disable_button3': disable_button, 
                 'credencial': credencial
             })
 
         # Se q2 existe mas q3 não, desabilita o botão de q3
         elif q2_exist and not q3_exist:
+            credencialglobal = credencial
             disable_buttonfalse = False
             disable_button = True
             return render(request, 'loginUser.html', {
-                'disable_button1': disable_button,
-                'disable_button2': disable_button,
-                'disable_button3': disable_buttonfalse,
+                'disable_button1': disable_button, 
+                'disable_button2': disable_button, 
+                'disable_button3': disable_buttonfalse, 
                 'credencial': credencial
             })
 
         # Se todos os questionários existem, exibe uma mensagem
         elif q3_exist:
+            credencialglobal = credencial
             return HttpResponse("Você já preencheu todos os questionários")
 
 
 def quest1(request):
-    return render(request, 'questionario1.html')
+    return render(request, 'questionario1.html', {'credencial': credencialglobal})
 
 
 def quest2(request):
-    return render(request, 'questionario2.html')
+    return render(request, 'questionario2.html', {'credencial': credencialglobal})
 
 
 def quest3(request):
-    return render(request, 'questionario3.html')
+    return render(request, 'questionario3.html', {'credencial': credencialglobal})
