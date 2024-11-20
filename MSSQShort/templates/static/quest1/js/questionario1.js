227b5967e83f9a18da5ccb9700b0f1f2bec45984
Maria Virginia Souza <mariavirginiasouza02@gmail.com>
2024-11-19 21:03:36 -0300

    ativando step 1

--- MSSQShort/templates/static/quest1/js/questionario1.js
+++ MSSQShort/templates/static/quest1/js/questionario1.js
@@ -1,48 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const inputs = document.querySelectorAll('input[type="text"]');
-  const radios = document.querySelectorAll('input[type="radio"]');
-  const submitButton = document.getElementById('botao');
+document.addEventListener('DOMContentLoaded', function () {
+    const inputs = document.querySelectorAll('input[type="text"]');
+    const radios = document.querySelectorAll('input[type="radio"]');
+    const submitButton = document.getElementById('botao');
 
-  // Configuração dos steps
-  $(document).ready(function () {
-    $(".form .button").click(function () {
-        const button = $(this);
-        const currentSection = button.parents(".section");
-    console.log("Seção Atual: ", currentSection);
-        const currentSectionIndex = currentSection.index();
-        const headerSection = $('.steps li').eq(currentSectionIndex);
-        const nextSection = currentSection.next();
-        console.log("Próxima Seção: ", nextSection);  
+    // Configuração dos steps
+    $(document).ready(function () {
+        $(".form .button").click(function () {
+            const button = $(this);
+            const currentSection = button.parents(".section");
+            console.log("Seção Atual: ", currentSection);
+            const currentSectionIndex = currentSection.index();
+            const headerSection = $('.steps li').eq(currentSectionIndex);
+            const nextSection = currentSection.next();
+            console.log("Próxima Seção: ", nextSection);
 
-        // Verifica se todos os campos da seção atual estão preenchidos
-        if (!validateCurrentSection(currentSection)) {
-            alert("Por favor, preencha todos os campos antes de continuar.");
-            return;
-        }
+            // Verifica se todos os campos da seção atual estão preenchidos
+            if (!validateCurrentSection(currentSection)) {
+                alert("Por favor, preencha todos os campos antes de continuar.");
+                return;
+            }
+            // Navegação entre seções
+            currentSection.removeClass("is-active").next().addClass("is-active");
+            headerSection.removeClass("is-active").next().addClass("is-active");
 
-$(".form .section").first().addClass("is-active");
-console.log($(".form .section").first());  // Verifique no console se a primeira seção está sendo selecionada corretamente
+            // Reinicia o formulário no final
+            if (currentSectionIndex === $('.form .section').length - 1) {
+                $(document).find(".form .section").first().addClass("is-active");
+                $(document).find(".steps li").first().addClass("is-active");
+            }
+        });
 
-
-        // Inicializa a visibilidade da primeira seção
-$(".form .section").first().addClass("is-active");
-
-// Navegação entre seções
-currentSection.removeClass("is-active").next().addClass("is-active");
-headerSection.removeClass("is-active").next().addClass("is-active");
-
-// Reinicia o formulário no final
-if (currentSectionIndex === $('.form .section').length - 1) {
-   $(document).find(".form .section").first().addClass("is-active");
-   $(document).find(".steps li").first().addClass("is-active");
-}
-});
-
-// Impede o envio padrão do formulário
-$(".form").submit(function (e) {
-e.preventDefault();
-});
-});
+        // Impede o envio padrão do formulário
+        $(".form").submit(function (e) {
+            e.preventDefault();
+        });
+    });
 
     // Valida campos da seção atual
     function validateCurrentSection(section) {
