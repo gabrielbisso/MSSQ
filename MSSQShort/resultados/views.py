e2d6da721d63a5737183f24eedf43080444d2873
Maria Eduarda Alves Gonzalez <104467663+meagonzalez@users.noreply.github.com>
2024-11-20 13:25:45 -0300

    Update views.py

--- MSSQShort/resultados/views.py
+++ MSSQShort/resultados/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import messages
 
-# Create your views here.
+
+def resultados(request):
+    if request.method == "GET":
+        return render(request, 'resultados.html')
+
+    elif request.method == "POST":
+        return HttpResponse("oIE moreh")
